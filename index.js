2fdc5474d4cb008c1e07236e325dc714951e13b2
Daniyal <165133375+programmerDani@users.noreply.github.com>
2024-04-06 16:52:46 +0500

    Add two consoles

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log("Wellcome in my number guessing game");
 const answer = await inquirer.prompt([
     {
         message: "Please guess a number between 1 and 10 : ",
@@ -14,3 +15,4 @@ if (answer.userGuessedNumber === randomNumber) {
 else {
     console.log("Try Again, you guessed a wrong number");
 }
+console.log("THE END");
